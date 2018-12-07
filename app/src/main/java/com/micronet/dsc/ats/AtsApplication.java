5564c9b7df1c95ac44550cd5d9265ae187346b64
skrstyen <scott.krstyen@micronet-inc.com>
2018-12-06 17:35:50 -0700

    Fixes for the config file and serial no
    Contains fixes for how the config file is being read in and how the
    serial number is being parsed when it is used by the engine class.
    
    For more information on both of these issues refer to issue #1 and #6
    
    Closes #1 and #6

--- app/src/main/java/com/micronet/dsc/ats/AtsApplication.java
+++ app/src/main/java/com/micronet/dsc/ats/AtsApplication.java
@@ -5,9 +5,15 @@
 
 package com.micronet.dsc.ats;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.Application;
 
+import android.content.Context;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by dschmidt on 1/26/17.
@@ -15,7 +21,7 @@ import java.util.Arrays;
 
 public class AtsApplication extends Application {
 
-    public static final String TAG = "ATS";
+    public static final String TAG = "ATS-Application";
 
     public AtsApplication() {
         // this method fires only once per application start.
@@ -29,15 +35,29 @@ public class AtsApplication extends Application {
         // this method fires once as well as constructor
         // but also application has context here
 
-
-
         Log.i(TAG, "Created: ATS version " + BuildConfig.VERSION_NAME);
 
         // if they exist we need to move the alternate configuration files to the primary location here
         //  before anything else in the application tries to access them
 
-        copyAlternateConfigFiles();
+        // Check which process this is: MainService, Io, or Watchdog because we don't want to update config files if
+        // it is Io or Watchdog
+        int pid = android.os.Process.myPid();
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
 
+        List<RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
+
+        if(processes != null){
+            for(RunningAppProcessInfo info: processes){
+
+                if(info.pid == pid && info.processName.equalsIgnoreCase("com.micronet.dsc.ats.obc5")){
+                    Log.i(TAG, "MainService is starting. Renaming new configuration and codemap files if present.");
+                    copyAlternateConfigFiles();
+                }
+            }
+        }else{
+            Log.e(TAG, "Running App Processes was null... alternate config files not copied if they were available.");
+        }
     }
 
 
